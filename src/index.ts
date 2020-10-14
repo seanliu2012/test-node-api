0c4dc7d9a57f26d497ab7deb004ee677f4ea180c
Sean Liu <sean.liu@myob.com>
2020-10-14 19:40:15 +1100

    let express router parse body into JSON

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import express from "express";
+import { app } from "./app";
 
-const app = express();
 const port = 8080 || process.env.PORT;
-
-app.get("/", (req, res) => {
-  res.send("Hi!");
-});
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
