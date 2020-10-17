a62e21e143f3bdf23bc830dc704b435d79c7b4d8
Sean Liu <sean.liu@myob.com>
2020-10-17 20:17:00 +1100

    implement CRUD endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,56 @@
 import express from "express";
-import * as bodyParser from "body-parser";
-import { connect } from "./db/db";
+import { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import { Movie } from "./db/models/Movie.model";
 
-connect();
-
+// create and setup express app
 const app = express();
-app.use(
-  bodyParser.json({
-    limit: "50mb",
-    verify(req: any, res, buf, encoding) {
-      req.rawBody = buf;
-    },
-  })
-);
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.json());
+
+// default route
+app.get("/", (req: Request, res: Response) => res.send("Hello!"));
+
+createConnection().then((connection) => {
+  // routes for resources in DB
+  app.post("/movies", async (req: Request, res: Response) => {
+    const movie = new Movie();
+    movie.title = req.body.title;
+    movie.plot_summary = req.body.plot_summary;
+    movie.duration = req.body.duration;
+    await movie.save();
+    res.send(movie);
+  });
+  app.get("/movies", async (req: Request, res: Response) => {
+    const movies = await Movie.find();
+    res.send(movies);
+  });
+  app.get("/movies/:id", async (req: Request, res: Response) => {
+    const movie = await Movie.findOne(req.params.id);
+    if (movie) {
+      res.send(movie);
+    } else {
+      res.status(404).send({ message: "Movie not found" });
+    }
+  });
+  app.put("/movies/:id", async (req: Request, res: Response) => {
+    const movie = await Movie.findOne(req.params.id);
+    if (movie) {
+      await Movie.merge(movie, req.body);
+      await movie.save();
+      res.send(movie);
+    } else {
+      res.status(404).send({ message: "Movie not found" });
+    }
+  });
+  app.delete("/movies/:id", async (req: Request, res: Response) => {
+    const movie = await Movie.findOne(req.params.id);
+    if (movie) {
+      await movie.remove();
+      res.send({ message: "Movie deleted" });
+    } else {
+      res.status(404).send({ message: "Movie not found" });
+    }
+  });
+});
 
 export { app };
